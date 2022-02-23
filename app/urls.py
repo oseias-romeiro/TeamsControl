1dd7c040ffb43db3c54eb567292796c400f46318
oseias-romeiro <oseiasromeirom@gmail.com>
2022-02-23 14:53:55 -0300

    index

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
-from pipes import Template
 from django.urls import path
-from django.views.generic import TemplateView
+
+from .views import TemplateView;
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name = 'index'),
+    path('signup/', TemplateView.as_view(template_name='signup.html'), name = 'signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
 
 # Create your views here.
