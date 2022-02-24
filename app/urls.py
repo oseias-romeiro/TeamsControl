36de947079b5f01c0db8269d430922a419b40236
oseias-romeiro <oseiasromeirom@gmail.com>
2022-02-24 00:22:22 -0300

    screens

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from .views import TemplateView;
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name = 'index'),
     path('signup/', TemplateView.as_view(template_name='signup.html'), name = 'signup'),
+    path('teamCreate/', TemplateView.as_view(template_name='teamCreate.html'), name = 'teamCreate'),
+    path('profile/', TemplateView.as_view(template_name='profile.html'), name = 'profile'),
+    path('team/', TemplateView.as_view(template_name='team.html'), name = 'team'),
 ]
