7f2e63500bc80802d7f2cc4d690f9d4396e8a9b1
oseias-romeiro <oseiasromeirom@gmail.com>
2022-02-25 10:30:33 -0300

    edit profile

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import TemplateView, CreateUserView;
+from .views import TemplateView, CreateUserView, EditProfile;
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name = 'index'),
     path('signup/', CreateUserView.as_view(), name = 'signup'),
     path('teamCreate/', TemplateView.as_view(template_name='teamCreate.html'), name = 'teamCreate'),
-    path('profile/', TemplateView.as_view(template_name='profile.html'), name = 'profile'),
+    path('profile/<int:pk>/', EditProfile.as_view()),
     path('team/', TemplateView.as_view(template_name='team.html'), name = 'team'),
 ]
+
+# error routes handler
+handler404 = TemplateView.as_view(template_name='404.html')
--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from app.forms import CustomUserCreate
+from app.forms import CustomUserCreate, CustomUserChange
+from .models import CustomUser
 
 class CreateUserView(CreateView):
     template_name = 'signup.html'
     success_url = reverse_lazy('login')
     form_class = CustomUserCreate
+
+class EditProfile(LoginRequiredMixin, UpdateView):
+    template_name = 'profile.html'
+    success_url = reverse_lazy('profile')
+    fields = ['nome_completo', 'interesse', 'linkedin']
+    model = CustomUser
